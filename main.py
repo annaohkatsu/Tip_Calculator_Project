dcb978f61b62d292a8107aca53cb7cd51d99e90d
annaohkatsu <ancyincanada@gmail.com>
2024-11-28 22:15:47 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15: "))
+
+total = bill + bill * tip /100
+
+people = int(input("How many people to split the bill? "))
+
+final = round((total / people),2)
+
+print(f"Each person should pay: ${final}")
